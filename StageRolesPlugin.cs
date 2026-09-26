6d68c55c4f133eab9e8b818484205f4ef7f147ae
CapacityDown <92231589+CapacityDown@users.noreply.github.com>
2026-09-26 19:57:14 +0900

    Start RoleShuffle v4.5.3 development

--- StageRolesPlugin.cs
+++ StageRolesPlugin.cs
@@ -17,7 +17,7 @@ public sealed class StageRolesPlugin : BaseUnityPlugin
 {
     public const string PluginGuid = "REPOJP.RoleShuffle";
     public const string PluginName = "RoleShuffle";
-    public const string PluginVersion = "4.5.2";
+    public const string PluginVersion = "4.5.3";
     public const string StageFluxGuid = "REPOJP.StagePhysicsEvents";
     public const string RepoConfigGuid = "nickklmao.repoconfig";
     public const string MenuLibGuid = "nickklmao.menulib";
